35fb0830d43e13f408db31e84d55a02c3bee86e1
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-05-26 21:21:31 +0200

    initial spring security configuration

---
+++ src/main/java/de/rwth/i9/palm/config/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+package de.rwth.i9.palm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+}
