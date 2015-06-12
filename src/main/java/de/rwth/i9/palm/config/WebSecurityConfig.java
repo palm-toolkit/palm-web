e1c2d062bf1df5981351fec436fe65420bbc4219
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-06-12 23:30:00 +0200

    updated security

--- src/main/java/de/rwth/i9/palm/config/WebSecurityConfig.java
+++ src/main/java/de/rwth/i9/palm/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
     		.csrf()
     			.disable()
             .authorizeRequests()
-.antMatchers( "/", "/home", "/login", "/logout", "/register", "/conference", "/researcher", "/publication", "/sparqlview/**" )
+.antMatchers( "/", "/home", "/login", "/logout", "/register", "/conference/**", "/researcher/**", "/publication/**", "/sparqlview/**" )
                 	.permitAll()
                 .anyRequest()
                 	.authenticated()
